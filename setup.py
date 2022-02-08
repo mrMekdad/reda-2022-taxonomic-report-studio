d4aaecd80da00eec1ddffaf1b06f507c03e3c996
mrMekdad <mekdadr6@gmail.com>
2022-02-08 18:09:00 +0100

    taxonomic-report-studio: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2022-taxonomic-report-studio",
+    version="0.1.0",
+    description="Studio-style report builder for taxonomic abundance summaries.",
+    author="Red@",
+    packages=["taxonomic_report_studio"],
+    package_dir={"": "src"},
+)
